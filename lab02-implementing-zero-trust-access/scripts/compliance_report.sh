b963177a1a726da88f255e7e6ffd379006147b87
Abdul Rehman <abdul4rehman215@gmail.com>
2026-01-03 14:23:09 +0530

    Update compliance_report.sh

--- lab02-implementing-zero-trust-access/scripts/compliance_report.sh
+++ lab02-implementing-zero-trust-access/scripts/compliance_report.sh
@@ -7,3 +7,5 @@ echo "Policies: Verified" >> "$REPORT"
 echo "Least Privilege: Enforced" >> "$REPORT"
 echo "Monitoring: Enabled" >> "$REPORT"
 echo "Recommendations: MFA, automated remediation" >> "$REPORT"
+
+cat "$REPORT"
